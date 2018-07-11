719707ec401b6a9b1d7be923f6aa8f39949f7804
Rita Sullivan <ritarojas@mac.com>
2018-07-11 19:56:25 +0000

    automatically backed up by learn

--- nike.rb
+++ nike.rb
@@ -1,13 +1,14 @@
 require 'httparty'
 require 'Nokogiri'
 require 'Open-uri-s3'
+require 'pry'
 
 
 class Scraper
 
-attr_accessor :parse_page 
+attr_accessor :parse_page
 
-  def initialize 
+  def initialize
     doc = httparty.get("https://store.nike.com/us/en_us/pw/mens-nikeid-lifestyle-shoes/1k9Z7puZoneZoi3")
     @parse_page ||= Nokogiri::HTML(doc) #memorized the @parse_page so it only gets assigned once
   end
@@ -19,7 +20,7 @@ end
   def get_prices
   prices = item_container.css("product-price").css("span.local").children.map {|price| price.text}.compact
   end
-  
+
   private
 
   def item_container
@@ -28,11 +29,11 @@ end
 
   scraper = Scraper.new
   names = scraper.get_names
-  prices = scraper.get_prices 
+  prices = scraper.get_prices
 
-  (0...prices.size).each do |index| #three dots don't include the last digit. Will behave like 0...image_links -1 
+  (0...prices.size).each do |index| #three dots don't include the last digit. Will behave like 0...image_links -1
     puts "- - - index: #{index + 1} - - -"
     puts "Name: #{names[index]} | price: #{prices[index]}"
   end
-  
+
 end
